440d17a9604591c26c8a9bc845c0a696349696de
Anatoly <akiselev57@mail.ru>
2022-03-29 17:46:21 +0300

    Правда или действие (часть 2)

--- js/formWorcks.js
+++ js/formWorcks.js
@@ -31,7 +31,7 @@ form.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   const isValid = pristine.validate();
-  if (isValid ) {
+  if (isValid) {
     console.log('Можно отправлять');
   } else {
     console.log('Форма невалидна');
@@ -39,15 +39,16 @@ form.addEventListener('submit', (evt) => {
 });
 
 function validateHashTag(value) {
-  const hashtags5 =value.split(' ');
- const valueBad =hashtags5.filter((item,index,array)=>{
-  return !re.test(item)||item.length>20||array.indexOf(item)!==index;
- });
-if (value ===''){
-  return true};
+  const hashtags5 = value.split(' ');
+  const valueBad = hashtags5.filter((item, index, array) => {
+    return !re.test(item) || item.length > 20 || array.indexOf(item) !== index;
+  });
+  if (value === '') {
+    return true;
+  };
 
- return (valueBad.length>0||hashtags5.length>5) ? false :true;
-  }
+  return (valueBad.length > 0 || hashtags5.length > 5) ? false : true;
+}
 
 
 pristine.addValidator(
